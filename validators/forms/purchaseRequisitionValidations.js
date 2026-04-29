96936662128fede1e0c268c9b78ae1172a001950
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-04-29 14:10:47 -0600

    feat: Quitar id de proyecto en validaciones de back

--- validators/forms/purchaseRequisitionValidations.js
+++ validators/forms/purchaseRequisitionValidations.js
@@ -1,7 +1,6 @@
 import { validateDate, validateDetailsArray, validateTextOptional, validateUUID } from "../fields/fieldsValidator.js";
 
 export const purchaseRequisitionValidation = [
-    validateUUID('projectId'),
     validateDate('requestDate'),
     validateTextOptional('observations'),
     validateDetailsArray
